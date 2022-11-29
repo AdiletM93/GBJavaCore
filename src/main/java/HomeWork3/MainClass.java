4ce512bb1a49181d67be10af5db47cd37400f484
AdiletM93 <m.adilet93@gmail.com>
2022-11-30 01:58:38 +0600

    HomeWork3

---
+++ src/main/java/HomeWork3/MainClass.java
@@ -0,0 +1,27 @@
+package HomeWork3;
+
+
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Box<Apple> appleBox = new Box<>();
+        appleBox.addFruit(new Apple(1.0f));
+        appleBox.addFruit(new Apple(1.0f));
+        appleBox.addFruit(new Apple(1.0f));
+        appleBox.addFruit(new Apple(1.0f));
+        appleBox.addFruit(new Apple(1.0f));
+        appleBox.addFruit(new Apple(1.0f));
+
+        Box<Orange> orangeBox = new Box<>();
+        orangeBox.addFruit(new Orange(1.5f));
+        orangeBox.addFruit(new Orange(1.5f));
+        orangeBox.addFruit(new Orange(1.5f));
+        orangeBox.addFruit(new Orange(1.5f));
+
+        System.out.println(appleBox.getWeight());
+        System.out.println(orangeBox.getWeight());
+
+        System.out.println(orangeBox.compare(appleBox));
+    }
+}
